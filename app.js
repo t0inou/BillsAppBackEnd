b67d05affdbc1a48c7b87e68b755e18f86924635
t0inou <antoine.hulin@viacesi.fr>
2019-11-12 16:04:56 +0100

    added imports for invoices/users in app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 var mysql = require('mysql');
+var users = require('./routes/users');
+var invoices = require('./routes/invoices');
+
 const app = express();
 
-app.use(function (req, res, next) {
+app.use(function (res, next) {
     res.locals.connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
@@ -13,8 +16,18 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.get('/', function (req, res) {
+app.use('/users', users);
+
+app.use('/invoices', invoices);
+
+app.get('/', function (res) {
     res.send('Hello')
+});
+
+app.user(function (res, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 app.listen(3000, function () {
