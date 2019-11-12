e5d9561d1d5707d5c29797871e43279d5ac59897
t0inou <antoine.hulin@viacesi.fr>
2019-11-12 15:46:24 +0100

    added connexion to DB MySQL

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+var mysql = require('mysql');
+const app = express();
+
+app.use(function (req, res, next) {
+    res.locals.connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'invoice'
+    });
+    res.locals.connection.connect();
+    next();
+});
 
 app.get('/', function (req, res) {
     res.send('Hello')
-})
+});
 
 app.listen(3000, function () {
     console.log('app listening on port 3000')
-})
+});
