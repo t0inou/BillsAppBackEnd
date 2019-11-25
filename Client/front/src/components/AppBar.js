1b43d60be62fd132439271b1df07d9e099221229
t0inou <antoine.hulin@viacesi.fr>
2019-11-25 10:56:23 +0100

    added react router with app bar

--- Client/front/src/components/AppBar.js
+++ Client/front/src/components/AppBar.js
@@ -5,6 +5,15 @@ import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
+import login from './login';
+import Users from './Users';
+import NewUser from './NewUser';
+import {
+    Route,
+    NavLink,
+    BrowserRouter as Router,
+    Switch
+} from "react-router-dom";
 
 const styles = theme => ({
     appBar: {
@@ -32,30 +41,37 @@ class AppBarComponent extends Component {
     render() {
         const { classes } = this.props;
         return (
-            <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
-                <Toolbar className={classes.toolbar}>
-                    <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
-                        Invoice App
+            <Router>
+                <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
+                    <Toolbar className={classes.toolbar}>
+                        <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
+                            Invoice App
             </Typography>
-                    <nav>
-                        <Link variant="button" color="textPrimary" href="#" className={classes.link}>
-                            Users
+                        <nav>
+                            <Link variant="button" color="textPrimary" href="#" className={classes.link}>
+                                Users
             </Link>
-                        <Link variant="button" color="textPrimary" href="#" className={classes.link}>
-                            Invoices
+                            <Link variant="button" color="textPrimary" href="#" className={classes.link}>
+                                Invoices
             </Link>
-                        <Link variant="button" color="textPrimary" href="#" className={classes.link}>
-                            New User
+                            <Link variant="button" color="textPrimary" href="#" className={classes.link}>
+                                <NavLink activeClassName="active" to="/NewUser">   New User</NavLink>
+                            </Link>
+                            <Link variant="button" color="textPrimary" href="#" className={classes.link}>
+                                New Invoice
             </Link>
-                        <Link variant="button" color="textPrimary" href="#" className={classes.link}>
-                            New Invoice
-            </Link>
-                    </nav>
-                    <Button href="#" color="primary" variant="outlined" className={classes.link}>
-                        Login
-                </Button>
-                </Toolbar>
-            </AppBar>
+                        </nav>
+                        <NavLink activeClassName="active" to="/login">   <Button color="primary" variant="outlined" className={classes.link}>
+                            Login
+                        </Button> </NavLink>
+                    </Toolbar>
+                </AppBar>
+                <Switch>
+                    <Route path="/login" component={login} />
+                    <Route path="/users" component={Users} />
+                    <Route path="/NewUser" component={NewUser} />
+                </Switch>
+            </Router>
         )
     }
 }
